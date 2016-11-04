8cdd647dcb04041715d0a48be454f4a11901913e
tunnckoCore <mameto_100@mail.bg>
2016-11-04 19:57:35 +0200

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+/*!
+ * minibase-results <https://github.com/node-minibase/minibase-results>
+ *
+ * Copyright (c) 2016 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+const utils = require('./utils')
+
+module.exports = (src, dest) => {
+
+}
---
+++ test.js
@@ -0,0 +1,10 @@
+/*!
+ * minibase-results <https://github.com/node-minibase/minibase-results>
+ *
+ * Copyright (c) 2016 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+/* jshint asi:true */
+
+'use strict'
