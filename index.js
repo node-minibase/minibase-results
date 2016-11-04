088b1d757fb0ca000031c3724b34b7ae7e175af4
tunnckoCore <mameto_100@mail.bg>
2016-11-04 20:32:37 +0200

    chore(inital): implement

--- index.js
+++ index.js
@@ -7,8 +7,29 @@
 
 'use strict'
 
-const utils = require('./utils')
+var through2 = require('through2')
+var extend = require('extend-shallow')
 
-module.exports = (src, dest) => {
+module.exports = function minibaseResults (opts) {
+  return function minibaseResults (self) {
+    self.options = extend({
+      serial: false,
+      settle: true,
+      showStack: false
+    }, self.options, opts)
 
+    self.define('tests', [])
+    self.define('testErrors', null)
+    self.define('testContext', { context: {} })
+    self.define('stream', through2.obj())
+    self.delegate({
+      pass: 0,
+      fail: 0,
+      skip: 0,
+      todo: 0,
+      count: 0,
+      runned: 0,
+      anonymous: 0
+    })
+  }
 }
--- test.js
+++ test.js
@@ -8,3 +8,53 @@
 /* jshint asi:true */
 
 'use strict'
+
+var test = require('mukla')
+var plugin = require('./index')
+var MiniBase = require('minibase').MiniBase
+
+test('should have default options - serial, showStack, settle', function (done) {
+  var app = new MiniBase()
+  app.use(plugin())
+  test.strictEqual(app.options.serial, false)
+  test.strictEqual(app.options.settle, true)
+  test.strictEqual(app.options.showStack, false)
+  done()
+})
+
+test('should be able to change options from MiniBase ctor', function (done) {
+  var app = new MiniBase({
+    settle: false,
+    showStack: true
+  })
+  app.use(plugin())
+  test.strictEqual(app.options.showStack, true)
+  test.strictEqual(app.options.settle, false)
+  done()
+})
+
+test('should be able to change options from plugin opts', function (done) {
+  var runner = new MiniBase()
+  runner.use(plugin({ serial: true }))
+  test.strictEqual(runner.options.serial, true)
+  done()
+})
+
+test('should have initial default properties', function (done) {
+  var app = new MiniBase()
+  app.use(plugin())
+  test.ok(app.tests)
+  test.ok(app.tests.length === 0)
+  test.ok(app.testErrors === null)
+  test.ok(app.testContext)
+  test.ok(app.testContext.context)
+  test.ok(app.stream)
+  test.strictEqual(app.pass, 0)
+  test.strictEqual(app.fail, 0)
+  test.strictEqual(app.skip, 0)
+  test.strictEqual(app.todo, 0)
+  test.strictEqual(app.count, 0)
+  test.strictEqual(app.runned, 0)
+  test.strictEqual(app.anonymous, 0)
+  done()
+})
